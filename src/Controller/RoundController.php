b10ff0b4f7600f6bfedc0f2d304bf8f5dd24eeb6
Simonas Kareiva <simonas@5grupe.lt>
2019-01-18 01:16:12 +0200

    Add two round routes in RoundController

--- src/Controller/RoundController.php
+++ src/Controller/RoundController.php
@@ -2,8 +2,9 @@
 
 namespace App\Controller;
 
-use App\Entity\Round;
 use App\Entity\Log;
+use App\Entity\Round;
+use App\Entity\QsoRecord;
 use App\Form\CallsignSearch;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -61,10 +62,59 @@ class RoundController extends AbstractController
         return $roundCheck;
     }
 
-        return $this->render('rounds/round.html.twig', [
+    /**
+     * @Route("/round/{date}/{callsign}", name="round_details", defaults={"date"=""})
+     */
+    public function getRoundLogsByCall($date,$callsign)
+    {
+        $dateCheck = $this->validateRound($date);
+        if ($dateCheck instanceof \Symfony\Component\HttpFoundation\Response) {
+            return $dateCheck;
+        }
+
+        $roundRepository = $this->getDoctrine()->getRepository(Round::class);
+        $callsignSearchForm = $this->createForm(CallsignSearch::class);
+        $qsoRecordRepository = $this->getDoctrine()->getRepository(QsoRecord::class);
+        $allRoundYears = $roundRepository->findAllRoundYears();
+        $roundLog = $qsoRecordRepository->getRoundLogByCallsign($date,$callsign);
+
+        if (empty($roundLog) && !empty($callsign)) {
+            return $this->redirectToRoute( 'round', array( 'date' => $date ) );
+        }
+
+        return $this->render('rounds/log.html.twig', [
             'round_years' => $allRoundYears,
+            'round_callsign' => $callsign,
+            'round_log' => $roundLog,
             'round_date' => $date,
-            'controller_name' => 'RoundController',
+            'callSearch' => $callsignSearchForm->createView(),
+        ]);
+
+    }
+
+    /**
+     * @Route("/round/{date}", name="round", defaults={"date"=""})
+     */
+    public function getRoundLogs($date)
+    {
+        $roundCheck = $this->validateRound($date);
+        if ($roundCheck instanceof \Symfony\Component\HttpFoundation\Response) {
+            return $roundCheck;
+        }
+
+        $roundRepository = $this->getDoctrine()->getRepository(Round::class);
+        $logRepository = $this->getDoctrine()->getRepository(Log::class);
+        $callsignSearchForm = $this->createForm(CallsignSearch::class);
+
+        $roundName = $roundCheck[0]->getName();
+        $roundYears = $roundRepository->findAllRoundYears();
+        $roundParticipants = $logRepository->findCallsignsByRoundDate($date);
+
+        return $this->render('rounds/round.html.twig', [
+            'round_years' => $roundYears,
+            'round_name' => $roundName,
+            'round_participants' => $roundParticipants,
+            'round_date' => $date,
             'callSearch' => $callsignSearchForm->createView(),
         ]);
 
