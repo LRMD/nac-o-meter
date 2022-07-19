1dba5a975c098d93c8373a027ea8e5cf4ed865b0
Simonas Kareiva <simonas@5grupe.lt>
2022-07-19 12:08:00 +0300

    Upgrade symfony to 4.3

--- src/Repository/CallsignRepository.php
+++ src/Repository/CallsignRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Callsign;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method Callsign|null find($id, $lockMode = null, $lockVersion = null)
@@ -14,7 +14,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class CallsignRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Callsign::class);
     }
--- src/Repository/RoundRepository.php
+++ src/Repository/RoundRepository.php
@@ -3,7 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Round;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Persistence\ManagerRegistry;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 
 /**
@@ -14,7 +14,7 @@ use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
  */
 class RoundRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Round::class);
     }
